5b33ce58fe7163908013042b1a36ebe469c64aa1
Steven Hollingsworth <shollingsworth@barracuda.com>
2017-08-08 20:31:56 -0700

    added support for centos 7
    I hate centos and how horribly out of date it is...
    but hey, at least it's stable

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 IFS=$'\n\t'
 mydir=$(dirname $0)
 
-err() {
-    echo "${1?"argv[1]: error text"}"
-    exit -1
-}
+release_file="/etc/os-release"
 
-packages+=("vim-pathogen")
+if [[ ! -e "${release_file}" ]]; then
+    echo "Error, cannot see my OS info in ${release_file}"
+    exit -1
+else
+    source /etc/os-release
+fi
+
+deb_pathogen="vim-pathogen"
+git_pathogen="https://github.com/tpope/vim-pathogen.git"
 
 clones+=("https://github.com/Valloric/YouCompleteMe.git")
 clones+=("https://github.com/davidhalter/jedi.git")
@@ -20,9 +25,63 @@ clones+=("https://github.com/Valloric/ycmd")
 clones+=("https://github.com/honza/vim-snippets.git")
 clones+=("https://github.com/Lokaltog/vim-easymotion")
 
-install_pathogen() {
-    sudo apt install ${packages[@]} || err "Error installing packages ${packages[@]}"
+err() {
+    echo "${1?"argv[1]: error text"}"
+    exit -1
 }
+
+check_git() {
+    which git &>/dev/null || err "Error, please install git first"
+}
+
+i_hate_centos_7() {
+    yum -y remove vim-enhanced vim-common vim-filesystem
+    yum clean all
+    yum -y groupinstall "Development tools"
+    yum -y install ncurses ncurses-devel
+    yum install -y ruby ruby-devel lua lua-devel luajit luajit-devel ctags git python python-devel python3 python3-devel tcl-devel perl perl-devel perl-ExtUtils-ParseXS perl-ExtUtils-XSpp perl-ExtUtils-CBuilder perl-ExtUtils-Embed
+    pushd /usr/local/src
+    test -d vim && rm -rfv vim
+    git clone https://github.com/vim/vim.git
+    pushd vim
+    make distclean
+    opts+=("--enable-python3interp")
+    opts+=("--enable-rubyinterp")
+    opts+=("--enable-luainterp")
+    opts+=("--enable-perlinterp")
+    opts+=("--enable-terminal")
+    opts+=("--enable-multibyte")
+    opts+=("--disable-gtktest")
+    opts+=("--disable-darwin")
+    opts+=("--disable-gpm")
+    opts+=("--prefix=/usr")
+    opts+=("--with-features=huge")
+    opts+=("--enable-pythoninterp")
+    #--with-python-config-dir=/usr/lib64/python2.7/config
+    ./configure ${opts[@]} || {
+       echo "Error compiling"
+       exit -1
+    }
+    make
+    make install
+}
+
+install_pathogen() {
+    case "${ID}" in
+        centos)
+            mkdir -p ~/repos
+            pushd ~/repos
+            #git clone ${git_pathogen}
+            clone_dir=$(echo ${git_pathogen} | awk -F '/' '{print $NF}' | sed 's/\.git$//')
+            cp -av ${clone_dir}/autoload ~/.vim
+            i_hate_centos_7
+            ;;
+        kali|ubuntu)
+            sudo apt install ${deb_pathogen} || err "Error installing packages ${deb_pathogen}"
+            ;;
+    esac
+}
+
 
 copy_files() {
     mkdir -pv ~/.vim/
@@ -44,7 +103,9 @@ submodule_updates() {
     done
 }
 
-install_pathogen
+check_git
+# order is important here
 copy_files
 install_plugins
 submodule_updates
+install_pathogen
